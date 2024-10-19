855abac8bf78c5e526e6448c555f8b4bcc868a3f
Telem Tobi <telem16@gmail.com>
2024-10-19 07:54:32 +0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Telem Tobi on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
